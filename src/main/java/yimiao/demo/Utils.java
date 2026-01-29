e6497a26a5b68b0f99cefdb2a64f9896b42e327a
yimiao <wangyilan@yonyou.com>
2026-01-29 12:16:46 +0800

    commit

---
+++ src/main/java/yimiao/demo/Utils.java
@@ -0,0 +1,54 @@
+package yimiao.demo;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Utils {
+    public static String sendGet(String url, Map<String, String> headers) {
+        // 创建默认的httpClient实例.
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        // 创建httpget
+        HttpGet httpGet = new HttpGet(url);
+        if (headers != null) {
+            for (String key : headers.keySet()) {
+                httpGet.addHeader(key, headers.get(key));
+            }
+        }
+        RequestConfig config = RequestConfig.custom().setConnectTimeout(1000) //连接超时时间
+                .setConnectionRequestTimeout(10 * 1000) //从连接池中取的连接的最长时间
+                .setSocketTimeout(30 * 1000) //数据传输的超时时间
+                .build();
+        //设置请求配置时间
+        httpGet.setConfig(config);
+
+        try {
+            CloseableHttpResponse response;
+            response = httpclient.execute(httpGet);
+            try {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    return EntityUtils.toString(entity, "UTF-8");
+                }
+            } finally {
+                response.close();
+            }
+        } catch (Exception e) {
+            return null;
+        } finally {
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
